cda50c21a96bdab8e0095b016ed0c70cb6918baa
Leonardo Camelo <leonardocamelo20@gmail.com>
2023-03-09 18:25:41 -0300

    .

--- Landing page/js/login.js
+++ Landing page/js/login.js
@@ -1,9 +1,15 @@
 function transition() {
     const buttonRegister = document.getElementById('register')
+    const buttonEnter = document.getElementById('enter')
 
     buttonRegister.addEventListener('click', () => {
       window.location = 'register.html'
     })
+
+    buttonEnter.addEventListener('click', () => {
+      window.location = 'interface.html'
+    })
+
 }
 
 transition()
