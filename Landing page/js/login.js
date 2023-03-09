bbbcf66bd52e09983bc985fc64f7873be9a7e418
Leonardo Camelo <leonardocamelo20@gmail.com>
2023-03-09 19:27:56 -0300

    Update login.js

--- Landing page/js/login.js
+++ Landing page/js/login.js
@@ -1,39 +1,39 @@
 function transition() {
-    const user = document.getElementById('nome')
-    const email = document.getElementById('email')
-    const senha = document.getElementById('senha')
+  const user = document.getElementById('nome')
+  const email = document.getElementById('email')
+  const senha = document.getElementById('senha')
 
-    const erro = document.getElementById('error')
+  const erro = document.getElementById('error')
 
-    const buttonRegister = document.getElementById('register')
-    const buttonEnter = document.getElementById('enter')
+  const buttonRegister = document.getElementById('register')
+  const buttonEnter = document.getElementById('enter')
 
-    const accout = [{
-      user: 'leonardo camelo',
-      email: 'leonardo123@gmail.com',
-      senha: 'userPassword'
-    }]
+  const accout = [{
+    user: 'leonardo camelo',
+    email: 'leonardo123@gmail.com',
+    senha: 'userPassword'
+  }]
 
-    // validação de entrada
+  // validação de entrada
 
-    buttonEnter.addEventListener('click', (e) => {
+  buttonEnter.addEventListener('click', (e) => {
 
-      e.preventDefault()
+    e.preventDefault()
 
-      if(user.value == accout[0].user && email.value == accout[0].email && senha.value == accout[0].senha) {
-        window.location = 'interface.html'
-      } else {
-        erro.style.display = 'flex'
-      }
+    if(user.value == accout[0].user && email.value == accout[0].email && senha.value == accout[0].senha) {
+      window.location = 'interface.html'
+    } else {
+      erro.style.display = 'flex'
+    }
 
 
-    })
+  })
 
-    // cadastrar
-    
-    buttonRegister.addEventListener('click', () => {
-      window.location = 'register.html'
-    })
+  // cadastrar
+  
+  buttonRegister.addEventListener('click', () => {
+    window.location = 'register.html'
+  })
 }
 
 transition()
