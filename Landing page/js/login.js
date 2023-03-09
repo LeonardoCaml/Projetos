3fe0dc68c326850eb3948b0a24349dcc11a02c9c
Leonardo Camelo <leonardocamelo20@gmail.com>
2023-03-09 19:23:04 -0300

    .

--- Landing page/js/login.js
+++ Landing page/js/login.js
@@ -1,15 +1,39 @@
 function transition() {
+    const user = document.getElementById('nome')
+    const email = document.getElementById('email')
+    const senha = document.getElementById('senha')
+
+    const erro = document.getElementById('error')
+
     const buttonRegister = document.getElementById('register')
     const buttonEnter = document.getElementById('enter')
 
+    const accout = [{
+      user: 'leonardo camelo',
+      email: 'leonardo123@gmail.com',
+      senha: 'userPassword'
+    }]
+
+    // validação de entrada
+
+    buttonEnter.addEventListener('click', (e) => {
+
+      e.preventDefault()
+
+      if(user.value == accout[0].user && email.value == accout[0].email && senha.value == accout[0].senha) {
+        window.location = 'interface.html'
+      } else {
+        erro.style.display = 'flex'
+      }
+
+
+    })
+
+    // cadastrar
+    
     buttonRegister.addEventListener('click', () => {
       window.location = 'register.html'
     })
-
-    buttonEnter.addEventListener('click', () => {
-      window.location = 'interface.html'
-    })
-
 }
 
 transition()
