2c1e6d83d4885f76a93a3d5f754b5124b51908f1
Leonardo Camelo <leonardocamelo20@gmail.com>
2023-03-17 17:06:26 -0300

    .

--- Web App - Finance/src/components/Form/index.js
+++ Web App - Finance/src/components/Form/index.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react"
+import Grid from "../Grid"
 import * as C from './style'
 
-function Form({ handleAdd })  {
+function Form({ handleAdd, transactionsList, setTransactionsList })  {
     const [desc, setDesc] = useState("")
     const [amount, setAmount] = useState("")
     const [isExpense, setExpense] = useState(false)
@@ -31,25 +32,42 @@ function Form({ handleAdd })  {
     }
 
     return (
-        <C.Container>
+        <>
+          <C.Container>
             <C.InputContent>
-                <C.Label>Descrição</C.Label>
-                <C.Input value={desc} onChange={(e) => setDesc(e.target.value)} />
+              <C.Label>Descrição</C.Label>
+              <C.Input value={desc} onChange={(e) => setDesc(e.target.value)} />
             </C.InputContent>
             <C.InputContent>
-                <C.Label>valor</C.Label>
-                <C.Input value={amount} onChange={(e) => setAmount(e.target.value)} />
+              <C.Label>Valor</C.Label>
+              <C.Input
+                value={amount}
+                type="number"
+                onChange={(e) => setAmount(e.target.value)}
+              />
             </C.InputContent>
             <C.RadioGroup>
-                <C.Input type='radio'id='rIncome' defaultChecked name="group1" onChange={() => setExpense(!isExpense)} />
-                <C.Label htmlFor="rExpenses">Entrada</C.Label>
-                <C.Input type='radio'id='rExpenses' defaultChecked name="group1" onChange={() => setExpense(!isExpense)} />
-                <C.Label htmlFor="rExpenses">Saída</C.Label>
+              <C.Input
+                type="radio"
+                id="rIncome"
+                defaultChecked
+                name="group1"
+                onChange={() => setExpense(!isExpense)}
+              />
+              <C.Label htmlFor="rIncome">Entrada</C.Label>
+              <C.Input
+                type="radio"
+                id="rExpenses"
+                name="group1"
+                onChange={() => setExpense(!isExpense)}
+              />
+              <C.Label htmlFor="rExpenses">Saída</C.Label>
             </C.RadioGroup>
             <C.Button onClick={handleSave}>ADICIONAR</C.Button>
-
-        </C.Container>
-    )
+          </C.Container>
+          <Grid itens={transactionsList} setItens={setTransactionsList} />
+        </>
+      )
 }
 
 export default Form
--- Web App - Finance/src/components/Grid/index.js
+++ Web App - Finance/src/components/Grid/index.js
@@ -4,6 +4,12 @@ import * as C from './styles'
 
 const Grid = ({ itens, setItens }) => {
 
+    const onDelete = (ID) => {
+        const newArray = itens.filter((transaction) => transaction.id !== ID)
+        setItens(newArray)
+        localStorage.setItem('transactions', JSON.stringify(newArray))
+    }
+
     return (
         <C.Table>
             <C.Thead>
@@ -16,7 +22,7 @@ const Grid = ({ itens, setItens }) => {
             </C.Thead>
             <C.Tbody>
                 {itens?.map((item, index) => (
-                    <GridItem key={index} item={item} />
+                    <GridItem key={index} item={item} onDelete={onDelete} />
                 ))}
             </C.Tbody>
         </C.Table>
